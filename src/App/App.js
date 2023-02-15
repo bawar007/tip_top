5fcafeb933b440521fdb7b7da3165a294ab47767
bawar007 <barski001@o2.pl>
2023-02-15 14:51:04 +0100

    add Navi with svg icons

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,11 @@
 import Header from "./Components/Header/Header";
+import Navi from "./Components/Navi/Navi";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Navi />
     </div>
   );
 }
---
+++ src/App/Components/Navi/Navi.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navi = () => {
+  return (
+    <div className="Navi">
+      <div className="Menu">
+        <img src="/icons/LOGO.svg" alt="logo" className="Logo" />
+        <img src="/icons/Home.svg" alt="home" className="Home" />
+        <img src="/icons/Galery.svg" alt="gallery" className="Gallery" />
+
+        <img src="/icons/Contact.svg" alt="contact" className="Contact" />
+        <img src="/icons/arrowcircleup.svg" alt="arrowUp" className="ArrowUp" />
+      </div>
+    </div>
+  );
+};
+
+export default Navi;
