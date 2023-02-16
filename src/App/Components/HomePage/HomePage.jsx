b82b0f7e81e21b2cb69a31cbc864b7a42912a5eb
bawar007 <barski001@o2.pl>
2023-02-16 19:56:00 +0100

    adding logo to home page

--- src/App/Components/HomePage/HomePage.jsx
+++ src/App/Components/HomePage/HomePage.jsx
@@ -1,5 +1,22 @@
 const HomePage = ({ homeRef }) => {
-  return <div className="homePage" ref={homeRef}></div>;
+  return (
+    <section className="homePage" ref={homeRef}>
+      <div>
+        <img src="/icons/LogoTipTopCss.svg" alt="logo" />
+      </div>
+      <div className="homeInfo">
+        <div className="info">
+          <h1>Tip</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+            maiores, corporis labore totam ipsam laborum nostrum iste
+            consectetur obcaecati, ratione vero cumque officia eveniet rerum
+            distinctio placeat dolorem est dolor?
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
