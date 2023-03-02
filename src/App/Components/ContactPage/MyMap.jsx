bd9eae58bb4560a58254685f330f2d26ce8e2b39
bawar007 <barski001@o2.pl>
2023-03-02 18:23:08 +0100

    adding map on contactPage

--- src/App/Components/ContactPage/MyMap.jsx
+++ src/App/Components/ContactPage/MyMap.jsx
@@ -1,5 +1,10 @@
-import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
-import { useContext } from "react";
+import {
+  GoogleMap,
+  InfoWindowF,
+  LoadScript,
+  MarkerF,
+} from "@react-google-maps/api";
+import { useContext, useState } from "react";
 import { AppContext } from "../../Provider/Provider";
 
 const containerStyle = {
@@ -20,6 +25,13 @@ const center = {
 
 const MyMap = () => {
   const { windowW } = useContext(AppContext);
+
+  const [info, setinfo] = useState(true);
+
+  const handleClickMarker = () => {
+    setinfo(true);
+  };
+
   return (
     <LoadScript googleMapsApiKey="AIzaSyDeVwoOw-20wXKQ9dkqN3aPhGShjYKPADQ">
       <GoogleMap
@@ -29,6 +41,7 @@ const MyMap = () => {
         zoom={17}
         options={{
           zoomControl: false,
+          streetViewControl: false,
         }}
       >
         <MarkerF
@@ -37,7 +50,32 @@ const MyMap = () => {
             lng: 20.276690939912914,
           }}
           animation={1}
-        />
+          onClick={handleClickMarker}
+        >
+          {info && (
+            <InfoWindowF
+              position={{
+                lat: 52.99440455242884,
+                lng: 20.276690939912914,
+              }}
+              onCloseClick={() => setinfo(false)}
+            >
+              <div className="infoBoxMap">
+                <h4>TIP - TOP, glazura i wykończenia wnętrz</h4>
+                <div className="infoBoxMapContent">
+                  <p>Kowalewko 48, 06-445 Strzegowo</p>
+                  <a
+                    href="https://goo.gl/maps/mR8iFw2kxzmN5Ldr8"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Wyznacz trasę
+                  </a>
+                </div>
+              </div>
+            </InfoWindowF>
+          )}
+        </MarkerF>
       </GoogleMap>
     </LoadScript>
   );
