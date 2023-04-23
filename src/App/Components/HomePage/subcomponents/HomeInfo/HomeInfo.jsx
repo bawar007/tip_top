3bd8391ee4834904f47267c6809ac1314611fa02
bawar007 <barski001@o2.pl>
2023-04-23 21:21:10 +0200

    fix paddings

--- src/App/Components/HomePage/subcomponents/HomeInfo/HomeInfo.jsx
+++ src/App/Components/HomePage/subcomponents/HomeInfo/HomeInfo.jsx
@@ -15,14 +15,14 @@ const HomeInfo = () => {
 
       <div className="info">
         <TitlePageLogo />
-        <p>
+        <span>
           <b>TIP-TOP</b> to firma, która jest zwięczeniem 15-letniego
           doświadczenia w glazurnictwie oraz wykończeniach wnętrz. Dzięki tak
           dużemu doświadczeniu nasze usługi są wykonywane profesjonalnie w
           dodatku szybko i solidnie. Głównym naszym celem jest kontakt z
           klientem, dzięki czemu współpraca na tej linii przebiega bez
           najmniejszych komplikacji.
-        </p>
+        </span>
       </div>
     </div>
   );
