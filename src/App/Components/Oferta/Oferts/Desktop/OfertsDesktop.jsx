737542a31da01f41b7c67a2d583b4ee15d376b27
bawar007 <barski001@o2.pl>
2023-04-24 21:23:55 +0200

    adding icons for oferts

--- src/App/Components/Oferta/Oferts/Desktop/OfertsDesktop.jsx
+++ src/App/Components/Oferta/Oferts/Desktop/OfertsDesktop.jsx
@@ -1,12 +1,21 @@
 import { oferts } from "../data/oferts";
 
 const OfertsDesktop = () => {
-  const ofertsEl = oferts.map((el) => (
+  const ofertsEl = oferts.map((el, index) => (
     <div className="multi-container--infoBox" key={el.id} id={`io${el.id}`}>
-      <h2>{el.title}</h2>
+      <h1>
+        {index === 0 ? <i className="fa fa-solid fa-bath"></i> : null}
+        {index === 1 ? <i className="fa fa-solid fa-building"></i> : null}
+        {el.title}
+      </h1>
       <ul className="infoBox_list">
         {el.items.map((el) => (
           <li key={el} className="infoBox_list--item">
+            <img
+              src="/tip_top/icons/measure.svg"
+              alt="da"
+              className="OfertListItem--img"
+            />
             {el}
           </li>
         ))}
