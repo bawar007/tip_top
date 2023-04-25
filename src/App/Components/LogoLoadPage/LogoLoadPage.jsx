954e35576b2889e59bfbc8a4bd947b2b4416d52a
bawar007 <barski001@o2.pl>
2023-04-25 21:08:25 +0200

    adding first page logo

---
+++ src/App/Components/LogoLoadPage/LogoLoadPage.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { AppContext } from "../../Provider/Provider";
+
+const LogoLoadPage = () => {
+  const { tip } = useContext(AppContext);
+  return (
+    <div className="LogoLoadPage">
+      <img
+        src={`${tip}/icons/LogoTipTopCss.svg`}
+        alt="logo"
+        className="LogoLoadPage--img"
+      />
+    </div>
+  );
+};
+
+export default LogoLoadPage;
