85a5e02adc470f0577bb86402539bf46cd72a904
bawar007 <barski001@o2.pl>
2023-05-22 22:50:17 +0200

    upgrade logopage

--- src/App/Components/LogoLoadPage/LogoLoadPage.jsx
+++ src/App/Components/LogoLoadPage/LogoLoadPage.jsx
@@ -1,8 +1,16 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AppContext } from "../../Provider/Provider";
 
 const LogoLoadPage = () => {
   const { tip } = useContext(AppContext);
+  useEffect(() => {
+    const homePage = document.querySelector(".homePage");
+    const logoPage = document.querySelector(".LogoLoadPage");
+    setTimeout(() => {
+      homePage.scrollIntoView();
+      setTimeout(() => logoPage.remove(), 1000);
+    }, 2500);
+  });
   return (
     <div className="LogoLoadPage">
       <img
