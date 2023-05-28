52e79b758c7e60b6cc506582c1a72bf8abc5998f
bawar007 <barski001@o2.pl>
2023-05-28 21:40:55 +0200

    add close modal when bg click

--- src/App/Components/Gallery/SubComponents/AllPicsPopUp/AllPicsPopup.jsx
+++ src/App/Components/Gallery/SubComponents/AllPicsPopUp/AllPicsPopup.jsx
@@ -1,7 +1,19 @@
+import { useContext, useEffect } from "react";
 import OnePicFromGallery from "./OnePicFromGallery/OnePicFromGallery";
 import AllPics from "./SubComponents/allPics";
+import { AppContext } from "../../../../Provider/Provider";
 
 const AllPicsPopUp = () => {
+  const { setAllPics, setAllPicsFromOpinion } = useContext(AppContext);
+  useEffect(() => {
+    const c = document.querySelector(".Selected_Pics_Modal--Bg");
+    window.onclick = (e) => {
+      if (e.srcElement === c) {
+        setAllPics(false);
+        setAllPicsFromOpinion(false);
+      }
+    };
+  });
   return (
     <div className="Selected_Pics_Modal--Bg">
       <div className="Selected_Pics_Modal--BOX">
