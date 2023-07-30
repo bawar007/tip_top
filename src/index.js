2a1687800411dca091b51be190290989d8afefea
bawar007 <barski001@o2.pl>
2023-07-30 18:11:36 +0200

    .

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/admin" element={<LoginPage />} />
